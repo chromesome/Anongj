7b4e3f5eaca93a7d97983f6eced6a33337a3ad26
Diegonzalez <71896076+chromesome@users.noreply.github.com>
2021-05-08 14:13:08 -0300

    Initial commit

---
+++ Assets/MenuController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MenuController : MonoBehaviourPunCallbacks
+{
+    //[SerializeField] private string versionName = "0.1";
+    [SerializeField] private GameObject usernameMenu;
+    [SerializeField] private GameObject controlPanel;
+    
+    [SerializeField] private InputField usernameInput;
+    [SerializeField] private InputField createGameInput;
+    [SerializeField] private InputField joinGameInput;
+
+    [SerializeField] private GameObject startButton;
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    private void Start()
+    {
+        usernameMenu.SetActive(true);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("Connected to game server");
+        //base.OnConnectedToMaster();
+    }
+
+    public void OnChangeUserNameInput()
+    {
+        if(usernameInput.text.Length > 3)
+        {
+            startButton.SetActive(true);
+        }
+        else
+        {
+            startButton.SetActive(false);
+        }
+    }
+
+    public void SetUserName()
+    {
+        usernameMenu.SetActive(false);
+        PhotonNetwork.NickName = usernameInput.text;
+    }
+
+    public void CreateGame()
+    {
+
+    }
+}
