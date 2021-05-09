ff82bc4cfef636b59ff84e44d6cc7f5f3e6ffe67
santiagoH626 <santiagohuerto.dj@outlook.es>
2021-05-09 13:25:30 -0500

    Anon

---
+++ Assets/Characters/AnonAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnonAnimator : MonoBehaviour
+{
+    public Vector2 mov;
+    Animator anim;
+    SpriteRenderer spr;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (mov.x!=0f){
+        	if (mov.x>0f){
+        		spr.flipX = false;
+        	} else {
+        		spr.flipX = true;	
+        	}
+        }
+        if (mov.magnitude>0f){
+        	anim.SetBool("moving", true);
+    	} else {
+    		anim.SetBool("moving", false);
+    	}
+    }
+}
---
+++ Assets/Characters/AnonController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnonController : MonoBehaviour
+{
+    public float runSp;
+
+    Rigidbody2D rb;
+    public Collider2D coll;
+
+    AnonAnimator aAnim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mov = new Vector2 (Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+    	rb.velocity = mov.normalized * runSp;
+    	aAnim.mov = mov.normalized;
+    }
+}
