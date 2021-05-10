860b3de668e6440b12d9c493a1cd8172dd00ad8b
Diegonzalez <71896076+chromesome@users.noreply.github.com>
2021-05-10 00:36:41 -0300

    sala de la partida
    cosas de multiplayer
    hermosa UI

---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject playerPrefab;
+    [SerializeField] GameObject gameCanvas;
+    [SerializeField] GameObject sceneCamera;
+
+    private void Awake()
+    {
+        gameCanvas.SetActive(true);
+    }
+
+    public void SpawnPlayer()
+    {
+        PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity, 0);
+        gameCanvas.SetActive(false);
+        //sceneCamera.SetActive(false);
+    }
+
+    public void LeaveGame()
+    {
+        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.LoadLevel("MainMenu");
+    }
+
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.WhyJam.Anongj
+{
+    public class MenuController : MonoBehaviourPunCallbacks
+    {
+        #region Singleton
+        public static MenuController Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+        #endregion
+
+        [SerializeField] private byte maxPlayers = 5;
+
+        [SerializeField] private GameObject usernameMenu;
+        [SerializeField] private GameObject controlPanel;
+    
+        [SerializeField] private InputField usernameInput;
+        [SerializeField] private InputField createGameInput;
+        [SerializeField] private InputField joinGameInput;
+
+        [SerializeField] private GameObject startButton;
+
+        [SerializeField] private RoomList roomContentList;
+
+        //private void Awake()
+        //{
+        //    PhotonNetwork.ConnectUsingSettings();
+        //}
+
+        private void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            usernameMenu.SetActive(true);
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            PhotonNetwork.JoinLobby(TypedLobby.Default);
+            Debug.Log("Connected to game server");
+            //base.OnConnectedToMaster();
+        }
+
+        public void OnChangeUserNameInput()
+        {
+            if(usernameInput.text.Length > 3)
+            {
+                startButton.SetActive(true);
+            }
+            else
+            {
+                startButton.SetActive(false);
+            }
+        }
+
+        public void SetUserName()
+        {
+            usernameMenu.SetActive(false);
+            PhotonNetwork.NickName = usernameInput.text;
+        }
+
+        public void CreateGame()
+        {
+            PhotonNetwork.CreateRoom(createGameInput.text, new RoomOptions() { MaxPlayers = maxPlayers }, null);
+        }
+
+        public void JoinGame()
+        {
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = maxPlayers;
+            PhotonNetwork.JoinOrCreateRoom(joinGameInput.text, roomOptions, TypedLobby.Default);
+        }
+
+        public void RefreshRoomList()
+        {
+            // TODO esto parece que no funciona, mejor manejarse solo con contraseña corte amongus?
+            PhotonNetwork.GetCustomRoomList(TypedLobby.Default, "");
+        }
+
+        public override void OnRoomListUpdate(List<RoomInfo> roomList)
+        {
+            base.OnRoomListUpdate(roomList);
+
+            roomContentList.AddList(roomList);
+        }
+
+        public void SelectGameRoom(RoomButton roomButton)
+        {
+            joinGameInput.text = roomButton.getRoomName();
+        }
+
+        public override void OnJoinedRoom()
+        {
+            base.OnJoinedRoom();
+            //PhotonNetwork.LoadLevel("MainGame");
+            PhotonNetwork.LoadLevel("MPTest");
+        }
+    }
+
+}
